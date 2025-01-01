35d564bdb7bd9b9352c0b0dcb55f0d5973a02c5c
andrejicd <andrejicd@gmail.com>
2025-01-01 14:15:19 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -5,3 +5,8 @@
 #define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID_WEATHER_STATION 1234567	          	//MathWorks weather station
+
+#define BACKLIGHT_ON_HOUR *        // Enter the hours to start
+#define BACKLIGHT_ON_MINUTE *      // Enter minutes to start
+#define BACKLIGHT_OFF_HOUR *       // Enter the hours for the end
+#define BACKLIGHT_OFF_MINUTE *     // Enter the minutes to the end
